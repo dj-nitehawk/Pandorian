f3506e68fbae50c9e7b2c79674cff9228ddc6099
djnitehawk <djnitehawk@DESKTOP>
2017-06-17 15:25:49 +0530

    fix: app version not reported properly

--- Pandorian/My Project/AssemblyInfo.vb
+++ Pandorian/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("Pandorian")> 
-<Assembly: AssemblyDescription("Custom client for pandora")> 
-<Assembly: AssemblyCompany("NiteHawk Creations")> 
-<Assembly: AssemblyProduct("Pandorian")> 
-<Assembly: AssemblyCopyright("www.djnitehawk.com")> 
-<Assembly: AssemblyTrademark("www.djnitehawk.com")> 
+<Assembly: AssemblyTitle("Pandorian")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Pandorian")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("bbf09e09-dd72-4842-9f55-b4ac7ab0213c")> 
+<Assembly: Guid("bbf09e09-dd72-4842-9f55-b4ac7ab0213c")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.0.0.0")>
+<Assembly: AssemblyFileVersion("2.8.1.0")>
