697bd8fc3717fca5d0a0815c5bbcca49a96afbe2
djnitehawk <djnitehawk@DESKTOP>
2017-07-04 18:13:24 +0530

    replay song on insufficient connectivity

--- Pandorian/My Project/AssemblyInfo.vb
+++ Pandorian/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("0.0.0.0")>
-<Assembly: AssemblyFileVersion("2.9.2.0")>
+<Assembly: AssemblyFileVersion("2.9.3.0")>
